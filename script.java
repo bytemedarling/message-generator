eb90f4133cf8c8c229f8e106f188038e88065786
bytemedarling <jessicakostyk@outlook.com>
2026-02-15 21:07:56 -0500

    html java and css added

---
+++ script.java
@@ -0,0 +1,19 @@
+const affirmations = [
+  "I am becoming stronger every day.",
+  "I am where I am meant to be.",
+  "I am brave, bold, and confident.",
+  "I deserve healthy relationships.",
+  "I release jealousy and comparing myself to others.",
+  "I am effortlessly attracting health, wealth, happiness, and love.",
+  "I am strong enough to rise above negative thoughts.",
+  "I am a survivor, a warrior, and a fighter.",
+  "I am made of power, strength, and resilience."
+];
+
+const button = document.getElementById("btn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * affirmations.length);
+  message.textContent = affirmations[randomIndex];
+});
